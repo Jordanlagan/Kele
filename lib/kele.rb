8c24b2018a13e179a424413ee79cfac09c02756e
Jordan Lagan <jordanlagan000@gmail.com>
2017-10-28 18:39:41 -0400

    Implemented #get_memethod that parses user data into Ruby

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -6,13 +7,19 @@ class Kele
   def initialize(email, password)
     @email = email
     @password = password
-    @base_api = "https://www.bloc.io/api/v1/sessions"
+    @base_api = "https://www.bloc.io/api/v1/"
 
-    response = self.class.post(@base_api, body: { email: @email, password: @password })
+    response = self.class.post(@base_api+"sessions", body: { email: @email, password: @password })
     if response.code == 404
       raise 'Invalid credentials'
     end
 
     @authentication_token = response["auth_token"]
+    @id = response["user"]["id"]
+  end
+
+  def get_me
+    response = self.class.get(@base_api+"users/me", headers: { "authorization" => @authentication_token })
+    JSON.parse(response.body)
   end
 end
