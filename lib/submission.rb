9548532caba6142146a6e6b9062aebd7d65c0259
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-01 09:20:13 -0400

    extra credit update submission

--- lib/submission.rb
+++ lib/submission.rb
@@ -7,4 +7,13 @@ module Submission
     )
     puts "submission sent" if response.success?
   end
+
+  def update_submission(id, assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+    response = self.class.post(@base_api+"checkpoint_submissions/"+id,
+      body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link, comment: comment,
+      enrollment_id: enrollment_id }, headers: { authorization: @authentication_token }
+    )
+    puts "submission sent" if response.success?
+  end
 end
