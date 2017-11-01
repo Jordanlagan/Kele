b12fb1c87b52ba19a450cd06383feb54567c828d
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-01 08:35:11 -0400

    Updated get_messages & create_message

--- lib/message.rb
+++ lib/message.rb
@@ -1,22 +1,20 @@
 module Message
-  def get_messages(page = 'all')
-    if !page == 'all'
-      response = self.class.get(@base_api+"message_threads", body: { :page => page },
+  def get_messages(page = nil)
+    if page.nil?
+      response = self.class.get(@base_api+"message_threads",
         headers: { "authorization" => @authentication_token }
       )
     else
       response = self.class.get(@base_api+"message_threads",
-        headers: { "authorization" => @authentication_token }
+        headers: { "authorization" => @authentication_token },  body: { "page" => page }
       )
     end
     @messages = JSON.parse(response.body)
   end
 
-  def create_message(recipient_id, token, subject, stripped_text)
-    response = self.class.post(@base_api+"messages", body: { :sender => @id,
-       :recipient_id => recipient_id, :token => token, :subject => subject,
-       "stripped-text" => stripped_text }, headers: { "authorization" => @authentication_token }
-       )
-    @created_message = JSON.parse(response.body)
+  def create_message(sender_email, recipient_id, stripped_text, subject )
+    response = self.class.post(@base_api+"messages", headers: { "authorization" => @authentication_token },
+               body: { sender: sender_email, recipient_id: recipient_id, "stripped-text" => stripped_text, subject: subject })
+    response.success? puts "message sent!"
   end
 end
