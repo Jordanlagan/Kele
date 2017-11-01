cf43af4eef8614db0728c83768812df868391acc
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-01 09:03:23 -0400

    fixed typo in submission.rb

--- lib/submission.rb
+++ lib/submission.rb
@@ -1,6 +1,6 @@
 module Submission
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
-    response = self.create.post(@base_api+"checkpoint_submissions",
+    response = self.class.post(@base_api+"checkpoint_submissions",
       body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch,
       assignment_commit_link: assignment_commit_link, comment: comment,
       enrollment_id: enrollment_id }, headers: { authorization: @authentication_token }
